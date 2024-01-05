215d19b52affcc8796dd3d88478411472f1cd5ad
GitHub Action <action@github.com>
2024-01-05 06:29:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/jiheedev/androidwizardplugins/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/jiheedev/androidwizardplugins/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jiheedev.androidwizardplugins.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/jiheedev/androidwizardplugins/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/jiheedev/androidwizardplugins/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.jiheedev.androidwizardplugins.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jiheedev.androidwizardplugins.MyBundle
+import com.github.jiheedev.androidwizardplugins.services.MyProjectService
 import javax.swing.JButton
 
 
